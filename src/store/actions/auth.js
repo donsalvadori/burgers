5052a6fa21eab70b69f37fff616efc77a8648001
Chris Salvi <csalvi42@gmail.com>
2019-03-20 20:36:26 -0700

    added spinner for login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
 	};
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
 	return {
 		type: actionTypes.AUTH_SUCCESS,
-		authData: authData
+		idToken: token,
+		userId: userId
 	};
 };
 
@@ -34,16 +35,16 @@ export const auth = (email, password, isSignup) => {
 		};
 		let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + API_KEY;
 		if (!isSignup) {
-			url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key='
+			url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + API_KEY;
 		}
 		axios.post(url, authData)
 				.then( response => {
 					console.log(response);
-					dispatch(authSuccess(response.data));
+					dispatch(authSuccess(response.data.idToken, response.data.localId));
 				})
 				.catch(err => {
 					console.log(err);
-					dispatch(authFail(err));
+					dispatch(authFail(err.response.data.error));
 				});
 	}
 }
