62b8ef59fcd62076e6e17176f422b9fe4c3f0953
Chris Salvi <csalvi42@gmail.com>
2019-03-22 22:12:25 -0700

    finished authentication pieces

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -28,6 +28,7 @@ const API_KEY = process.env.REACT_APP_AUTH_API_KEY;
 export const logout = () => {
 	localStorage.removeItem('token');
 	localStorage.removeItem('expirationDate');
+	localStorage.removeItem('userId');
 	return {
 		type: actionTypes.AUTH_LOGOUT
 	};
@@ -60,6 +61,7 @@ export const auth = (email, password, isSignup) => {
 					const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
 					localStorage.setItem('token', response.data.idToken);
 					localStorage.setItem('expirationDate', expirationDate);
+					localStorage.setItem('userId', response.data.localId);
 					dispatch(authSuccess(response.data.idToken, response.data.localId));
 					dispatch(checkAuthTimeout(response.data.expiresIn));
 				})
@@ -75,6 +77,25 @@ export const setAuthRedirectPath = (path) => {
 		type: actionTypes.SET_AUTH_REDIRECT_PATH,
 		path: path
 	}
+}
+
+export const authCheckState = () => {
+	return dispatch => {
+		const token = localStorage.getItem('token');
+		if (!token) {
+			dispatch(logout());
+		} else {
+			const expirationDate = new Date(localStorage.getItem('expirationDate'));
+			if (expirationDate <= new Date()) {
+				dispatch(logout());
+			} else {
+				const userId = localStorage.getItem('userId');
+				dispatch(authSuccess(token, userId));	
+				dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+			}
+			
+		}
+	};
 }
 
 
